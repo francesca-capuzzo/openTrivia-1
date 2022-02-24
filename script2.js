9e564804cb91e2cb1a23e7c26f06f9dddc5e520d
JMIKEPAD <mchparodi@gmail.com>
2022-02-24 11:19:44 +0100

    update: add gif and modify style, script2

--- script2.js
+++ script2.js
@@ -5,6 +5,7 @@ function loadTrivia() {
     .catch(error => console.log(error));
 }
 
+let points = 0;
 
 function createTrivias(data) {                     
     const results = data.results;                  
@@ -18,6 +19,7 @@ function createTrivias(data) {
 
 
 function createDivQuestion(trivia) {
+   
     let question = document.createElement('div');
     question.className = 'question-div'
 
@@ -25,13 +27,13 @@ function createDivQuestion(trivia) {
     span.className = 'question-span';
     span.style.display = 'block';
 
-    let textNode = document.createTextNode(trivia.question.replace(/&quot/g, '"').replace(/&#039/g, "'").replace(/;/g, "").replace(/&rsquo/g,"'"));
+    let textNode = document.createTextNode(trivia.question.replace(/&quot/g, '"').replace(/&#039/g, '"').replace(/;/g, "").replace(/&rsquo/g, "'").replace(/&amp/g, ' & ').replace(/&egrave/g, 'è').replace(/&eacute/g, 'é').replace(/&rdquo/g, '"').replace(/&aacute/g, 'à').replace(/&uacute/g, 'ù').replace(/&ouml/g,'ö' ));
     
     span.appendChild(textNode);
     question.appendChild(span);
 
     for (const answer of trivia.getAllAnswers()) {
-        let buttonAnswer = createButtonAnswer(answer);
+        let buttonAnswer = createButtonAnswer(answer, trivia, question);
         question.appendChild(buttonAnswer);
     }
     return question;
@@ -40,21 +42,74 @@ function createDivQuestion(trivia) {
 
 function displayTrivia(triviaArray) {
     let mainDiv = document.getElementById('main-container');
+    const userPoints = document.createElement('div');
+    userPoints.className = 'points'
+    let textNode = document.createTextNode('User Points:'+ " " + points + " / 10");
+    
+    
 
     for (const question of triviaArray) {
         let divQuestion = createDivQuestion(question);
         mainDiv.appendChild(divQuestion);
     }
+    userPoints.appendChild(textNode);
+    mainDiv.appendChild(userPoints);
     return mainDiv;
 }
 
 
-function createButtonAnswer(answer) {
+
+// let flag = false;
+
+function createButtonAnswer(answer, trivia, question) {
     let answerButton = document.createElement('button');
-    let textNode = document.createTextNode(answer.replace(/&quot/g, '"').replace(/&#039/g, "'").replace(/;/g, "").replace(/&rsquo/g,"'"));
+    answerButton.className = 'button'
+    let textNode = document.createTextNode(answer.replace(/&quot/g, "'").replace(/&#039/g, "'").replace(/;/g, "").replace(/&rsquo/g, "'").replace(/&amp/g, ' & ').replace(/&egrave/g, 'è').replace(/&eacute/g, 'é').replace(/&rdquo/g, '"').replace(/&aacute/g, 'à').replace(/&uacute/g, 'ù').replace(/&ouml/g,'ö' ));
     answerButton.appendChild(textNode);
+    
+    // if (flag === false) {
+    //     answerButton.addEventListener('click',(event) => buttonClick(event, trivia, question));
+    // }else {
+    //     answerButton.removeEventListener('click',(event) => buttonClick(event, trivia));
+    // }
+    
+    answerButton.addEventListener('click',(event) => buttonClick(event, trivia, question));
 
     return answerButton;
+}
+
+
+
+
+function buttonClick(event, trivia, question) {
+    let text = event.originalTarget.firstChild.textContent;
+    
+    let correct = trivia.checkAnswer(text);
+   
+    if (correct) {
+        points + 1;
+        event.originalTarget.style.backgroundImage = 'linear-gradient(to left, rgb(238, 130, 238), rgb(75, 0, 130), rgb(0, 0, 255), rgb(0, 128, 0), rgb(255, 165, 0), rgb(255, 0, 0))';
+        // event.originalTarget.style.backgroundColor = 'green';
+        event.originalTarget.style.color = 'white';
+        // flag = true;
+        //event.originalTarget.removeEventListener('click', (event) => buttonClick(event, trivia));
+      points = points + 1 
+    }
+    else{
+        points = points;
+        event.originalTarget.style.backgroundColor = 'red';
+        event.originalTarget.style.color = 'black';
+        // flag = true;
+        //event.originalTarget.removeEventListener('click', (event) => buttonClick(event, trivia)); 
+    }
+    console.log(points);
+    const buttons = question.getElementsByTagName('button');
+
+    
+    for (let i = 0; i < buttons.length; i++) {
+        
+        buttons[i].outerHTML = buttons[i].outerHTML; //removEventListener non funziona quindi prendo outerHTML che è l'elemento senza funzioni (solo html e css) e lo metto uguale a se stesso
+    }
 }
 
 
--- trivia.js
+++ trivia.js
@@ -1,24 +1,36 @@
-class Trivia{
-    constructor(category, type, difficulty, question, correctAnswer, incorrectAnswer ){
-      this.category = category;
-      this.type = type;
-      this.difficulty = difficulty;
-      this.question = question;
-      this.correctAnswer = correctAnswer;
-      this.incorrectAnswer = incorrectAnswer;
-    }
+class Trivia {
+  constructor(category, type, difficulty, question, correctAnswer, incorrectAnswer) {
+    this.category = category;
+    this.type = type;
+    this.difficulty = difficulty;
+    this.question = question;
+    this.correctAnswer = correctAnswer;
+    this.incorrectAnswer = incorrectAnswer;
+  }
 
 
-//siccome nell'oggetto trivia le risposte giuste e sbagliate sono separate, le metto insieme.
-    getAllAnswers(){
-      const allAnswers = [];
-      allAnswers.push(this.correctAnswer);
-      allAnswers.push(...this.incorrectAnswer);             //perchè incorrect answers è già un array
-      // for (const answ of this.incorrectAnwers) {         //questo è un altro modo per pushare l'array spreaddato nell'array allanswers ma è la stessa cosa --> volendo si può usare anche CONCAT per lo stesso purpose
-      //   allAnswers.push(answ)
-      // }
-      const shuffleArray = Utility.shuffleArray(allAnswers);  //utilizzo lo shuffleArray che è nella classe Utility. --> per evitare che la risporta n^1 sia sempre la corretta.
-      return shuffleArray;                                    //restituisce un array che contiene tutte le risposte, corrette e sbagliate
+  //siccome nell'oggetto trivia le risposte giuste e sbagliate sono separate, le metto insieme.
+  getAllAnswers() {
+    const allAnswers = [];
+    allAnswers.push(this.correctAnswer);
+    allAnswers.push(...this.incorrectAnswer);             //perchè incorrect answers è già un array
+    // for (const answ of this.incorrectAnwers) {         //questo è un altro modo per pushare l'array spreaddato nell'array allanswers ma è la stessa cosa --> volendo si può usare anche CONCAT per lo stesso purpose
+    //   allAnswers.push(answ)
+    // }
+    const shuffleArray = Utility.shuffleArray(allAnswers);  //utilizzo lo shuffleArray che è nella classe Utility. --> per evitare che la risporta n^1 sia sempre la corretta.
+    return shuffleArray;                                    //restituisce un array che contiene tutte le risposte, corrette e sbagliate
+  }
+
+
+  checkAnswer(answer){
+    if (answer === this.correctAnswer) {
+      return true;
     }
+    else{
+      return false;
+    }
+  }
+
+
 }
 
